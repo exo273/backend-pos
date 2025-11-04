179db3510fe672b47142a4359be6a54fc81a29eb
exo273 <exo273@users.noreply.github.com>
2025-11-03 23:05:20 -0300

    Fix: Cambiar DB_PASS a DB_PASSWORD en settings.py

--- orders_service/settings.py
+++ orders_service/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': os.getenv('DB_NAME', 'db_pos'),
         'USER': os.getenv('DB_USER', 'root'),
-        'PASSWORD': os.getenv('DB_PASS', ''),
+        'PASSWORD': os.getenv('DB_PASSWORD', ''),
         'HOST': os.getenv('DB_HOST', 'localhost'),
         'PORT': os.getenv('DB_PORT', '3306'),
         'OPTIONS': {
